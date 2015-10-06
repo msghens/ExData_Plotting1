f2a9824c5d15a739b144f3319262b7718e43737f
Michael Ghens <michael@spconnect.com>
2015-10-06 00:05:24 -0700

    Plot1 code and graphic

--- plot1.R
+++ plot1.R
@@ -3,6 +3,20 @@
 
 
 #Read Data
-plotData <- read.csv(file = "household_power_consumption.txt", na.strings = "?", sep = ";")
+plotData <- read.csv(file = "household_power_consumption.txt", na.strings = "?", sep = ";", stringsAsFactors = FALSE)
 
 
+#Convert date from chr to date
+
+plotData$Date <- as.Date(plotData$Date,"%d/%m/%Y")
+
+#subset data for dates between 2007-02-01 and 2007-02-02
+
+plotData <- subset(plotData,Date >= "2007-02-01" & Date <= "2007-02-02")
+
+hist(plotData$Global_active_power,col='red',xlab = "Global Active Power (kilowatts)",main = "Global Active Power")
+
+# Save it to file
+
+dev.copy(png,file = "plot1.png")
+dev.off()
